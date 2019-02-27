dd42161ebd4ebdf5485d9b4c4183a96c4f99b5c3
anushil05 <markanushil@gmail.com>
2019-02-27 15:26:53 +0530

    adding the react material

---
+++ HelloReact/public/app.jsx
@@ -0,0 +1,21 @@
+//Creating a react component
+//React createClass takes one and only argument an options object
+//Here we decide the behaviour of our component
+//The only thing that is required of every react component is a render method
+//All th render method excepts some jsx code that we can render to the browser
+//We have to return only one root html element here div is the only root element
+var Greater =  React.createClass({
+    render : function () {
+        return (
+            <div>
+                <h1>Hello React!</h1>
+                <p>This is from, Greater Component</p>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <Greater/>, 
+    document.getElementById('app')
+);
---
+++ HelloReact/server.js
@@ -0,0 +1,19 @@
+//First node file
+//for excess to entire express api
+var express = require('express');
+
+//Create our app
+var app = express();
+
+//to start a server
+//1. Tell it which folder we want to serve
+
+//app.use() help us to add functionality to our express application
+//express.static() it we specify a folder name that we want to expose to the web server
+app.use(express.static('public'));
+
+//2. Start The server
+//it takes the port Node, and the function that it will call
+app.listen(3000, function () {
+    console.log('Express Server is up on port 3000');
+});
