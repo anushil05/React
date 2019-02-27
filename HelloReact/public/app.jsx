16a297ab6443ae96088adf3ac2a8727fa43dfde3
anushil05 <markanushil@gmail.com>
2019-02-27 17:33:36 +0530

    User Events & Callbacks & Component State

--- HelloReact/public/app.jsx
+++ HelloReact/public/app.jsx
@@ -11,15 +11,42 @@ var Greater = React.createClass({
             message: 'This is deafult message for React Component',
         }
     },
+    getInitialState: function () {
+        return {
+            name: this.props.name
+        }
+    },
+    onButtonClick: function (e) {
+        e.preventDefault(); //Prevent the form from submiiting and causing the refersh, pprevent full browser reload
+
+        var nameRef = this.refs.name;
+        var name = nameRef.value; //this represents the browser node and to get the value we use .value
+        //the react update only that part in which state is changes, 
+        //so we can see the value in text box even after update
+        //to clear the text from the browser
+        nameRef.value = ""; 
+
+        //for not updating with null string
+        if (typeof name === "string" && name.length > 0) {
+            this.setState({
+                name: name
+            });
+        }
+    },
     render: function () {
         //Now to access the props this.props object
         //it stores all of our props
-        var name = this.props.name;
+        var name = this.state.name;
         var message = this.props.message;
         return (
             <div>
                 <h1>Hello {name}! </h1>
                 <p>{message + '!!'}</p>
+
+                <form onSubmit={this.onButtonClick}>
+                    <input type="text" ref="name"/>
+                    <button>Set Name</button>
+                </form>
             </div>
         );
     }
